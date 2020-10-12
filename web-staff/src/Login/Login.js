b117b226821aca3c19d6617d4ab0c8e905e82b1f
LAPTOP-UI8QJVQ5\HOME <phuongcf1998@gmail.com>
2020-10-12 21:16:35 +0700

    Add remember me in login page

--- web-staff/src/Login/Login.js
+++ web-staff/src/Login/Login.js
@@ -14,6 +14,7 @@ class Login extends React.Component {
       Password: "",
       token: "",
       loginError: false,
+      rememberMe: false,
     };
 
     this.Password = this.Password.bind(this);
@@ -21,6 +22,16 @@ class Login extends React.Component {
     this.Email = this.Email.bind(this);
 
     this.login = this.login.bind(this);
+
+    this.rememberMe = this.rememberMe.bind(this);
+  }
+
+  componentDidMount() {
+    const rememberMe = localStorage.getItem("rememberMe") === "true";
+    const email = rememberMe ? localStorage.getItem("email") : "";
+    const password = rememberMe ? localStorage.getItem("password") : "";
+    this.setState({ Email: email, Password: password, rememberMe });
+    rememberMe ? document.getElementById("isRemember").checked = true : document.getElementById("isRemember").checked = false;
   }
 
   Email(event) {
@@ -30,21 +41,31 @@ class Login extends React.Component {
   Password(event) {
     this.setState({ Password: event.target.value });
   }
+  rememberMe(event) {
+    this.setState({ rememberMe: event.target.checked });
+    console.log(event.target.checked);
+  }
 
   login(event) {
+    const { Email, Password, rememberMe } = this.state;
     axios
       .post("https://reqres.in/api/login", {
         email: this.state.Email,
         password: this.state.Password,
       })
       .then((res) => this.setState({ token: res.data }))
+      .then((res) => localStorage.setItem("rememberMe", rememberMe))
+      .then((res) =>
+        localStorage.setItem("email", this.state.rememberMe ? Email : "")
+      )
+      .then((res) =>
+        localStorage.setItem("password", this.state.rememberMe ? Password : "")
+      )
+
       .catch((err) => this.setState({ loginError: true }));
   }
 
-  
-
   render() {
-    
     if (this.state.token !== "") {
       sessionStorage.setItem("userToken", this.state.token);
       return <Redirect to={{ pathname: "/home", state: this.state.token }} />;
@@ -59,13 +80,19 @@ class Login extends React.Component {
             type="text"
             placeholder="User Name"
             onChange={this.Email}
+            value={this.state.Email}
           />
           <input
             className="form-input"
             type="password"
             placeholder="Password"
             onChange={this.Password}
+            value={this.state.Password}
           />
+          <label>
+            <input id="isRemember" type="checkbox" onChange={this.rememberMe}></input> Remember
+            me
+          </label>
           <button
             className="button-login"
             type="submit"
@@ -74,7 +101,9 @@ class Login extends React.Component {
           >
             Login
           </button>
-          {this.state.loginError ? <p style={{ color: "red" }}>Invalid username or password</p> : null}
+          {this.state.loginError ? (
+            <p style={{ color: "red" }}>Invalid username or password</p>
+          ) : null}
         </div>
       </div>
     );
